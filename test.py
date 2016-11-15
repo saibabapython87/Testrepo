bb1259693a82c29bfdd719303d38276cd3ed515b
saibabapython87 <saibabapython87@gmail.com>
2016-11-15 23:05:24 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,15 +2,44 @@ import json, requests
 res = requests.get('https://developer.apple.com/videos/play/wwdc2016/')
 video_data = json.loads(res.text)
 obj_final = {}
+if val['year']=input("Enter any year as integer: ")
 for val in video_data:
     if val['year'] == 2016:
 	if val.values() == '':
 	    null_obj = None
-	    obj_final.update(null_obj)
-	    print "Video content for 2016 if null : ",obj_final
+	    print "Video content for 2016 if null : ",obj_final.update(null_obj)
 	else:
 	    print "Video content for 2016 : ",vals
+    elif val['year'] == 2017:
+	if val.values() == '':
+	    null_obj = None
+	    print "Video content for 2017 if null : ",obj_final.update(null_obj)
+	else:
+	    print "Video content for 2017 : ",vals
+    elif val['year'] == 2018:
+	if val.values() == '':
+	    null_obj = None
+	    print "Video content for 2018 if null : ",obj_final.update(null_obj)
+	else:
+	    print "Video content for 2018 : ",vals
+    if val['year'] == 2019:
+	if val.values() == '':
+	    null_obj = None
+	    print "Video content for 2019 if null : ",obj_final.update(null_obj)
+	else:
+	    print "Video content for 2019 : ",vals
+    if val['year'] == 2020:
+	if val.values() == '':
+	    null_obj = None
+	    print "Video content for 2020 if null : ",obj_final.update(null_obj)
+	else:
+	    print "Video content for 2020 : ",vals
+    else:
+        print "You entered year data is not vailable"
 
 # Clone the repositary
 # To run the script - python test.py
-# Here I have written the code how we can make it for sudo code, Kindly check for the same.
+
+
+
+
